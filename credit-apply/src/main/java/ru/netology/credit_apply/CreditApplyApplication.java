5d75ee1c54901dd54980b9eb1d93d82551fbfc52
Vadim Persidskii <vadim@example.com>
2025-05-02 17:36:28 +0300

    First microservice (applier) - initial commit

---
+++ credit-apply/src/main/java/ru/netology/credit_apply/CreditApplyApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.credit_apply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditApplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditApplyApplication.class, args);
+	}
+
+}
