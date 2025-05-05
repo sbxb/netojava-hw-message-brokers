b797fef0f08608dac130b79a298289be2cfc87ef
Vadim Persidskii <vadim@example.com>
2025-05-05 17:52:04 +0300

    First microservice (applier) - update application status upon receiving event from processor microservice

--- credit-apply/src/main/java/ru/netology/credit_apply/service/RabbitMQReceiver.java
+++ credit-apply/src/main/java/ru/netology/credit_apply/service/RabbitMQReceiver.java
@@ -3,14 +3,27 @@ package ru.netology.credit_apply.service;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import ru.netology.credit_apply.repository.LoanApplicationRepository;
 import ru.netology.credit_common.model.LoanApplicationProcessorEvent;
 
 @Service
 public class RabbitMQReceiver {
     private final String mainQueue = "laq";
+    private final LoanApplicationRepository repo;
+
+    public RabbitMQReceiver(LoanApplicationRepository repo) {
+        this.repo = repo;
+    }
 
     @RabbitListener(queues = mainQueue)
     public void processOrder(LoanApplicationProcessorEvent e) {
-        System.out.println("!!! " + e);
+        var applicationOptional = repo.findById(e.id());
+        if (applicationOptional.isPresent()) {
+            var application = applicationOptional.get();
+            application.setStatus(e.status());
+            repo.save(application);
+        } else {
+            System.out.println("WARNING got status update event for missing application id " + e.id());
+        }
     }
 }
